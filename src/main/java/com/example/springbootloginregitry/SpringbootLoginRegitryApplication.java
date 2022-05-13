3c7424437965cb3ee24d5301fae465cda720ee49
wangwei <412390898@qq.com>
2022-05-13 15:09:21 +0800

    创建项目

---
+++ src/main/java/com/example/springbootloginregitry/SpringbootLoginRegitryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootloginregitry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLoginRegitryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLoginRegitryApplication.class, args);
+    }
+
+}
